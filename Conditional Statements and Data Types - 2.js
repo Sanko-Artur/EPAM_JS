28462db8a35f13c24ba1733173caece2aa60dc01
Артур <87715183+Sanko-Artur@users.noreply.github.com>
2021-10-31 18:43:49 +0300

    Add files via upload

---
+++ Conditional Statements and Data Types - 2.js
@@ -0,0 +1,21 @@
+/*
+Выполнить умножение различных типов(string * boolean, string * number, number * boolean)
+ */
+
+let string = 'привет';
+let bool1 = true;
+let bool2 = false;
+let x = string * bool1;
+console.log(`${x}`);
+let x1 = string * bool2;
+console.log(`${x1}`);
+
+let str = 'hi!';
+let num = 5;
+let sum = str * num;
+console.log(`${sum}`);
+
+let x3 = num * bool1;
+console.log(`${x3}`);
+let x4 = num * bool2;
+console.log(`${x4}`);
