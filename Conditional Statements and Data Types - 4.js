6ba92fd1477e978e98a2af3e9320ed50f2b17c89
Артур <87715183+Sanko-Artur@users.noreply.github.com>
2021-11-01 20:27:14 +0300

    Add files via upload

---
+++ Conditional Statements and Data Types - 4.js
@@ -0,0 +1,12 @@
+/*
+Выполнить явное преобразование(number, string, boolean)
+ */
+let n = 5;
+let str = "5";
+let str1 = '';
+let bool = true;
+
+console.log(n);
+console.log(+str);
+console.log(+str1);
+console.log(+bool);
