0226332d9a31da8457519ace2ccf00b4ccb08e6b
Артур <87715183+Sanko-Artur@users.noreply.github.com>
2021-11-01 22:06:08 +0300

    Add files via upload

--- Conditional Statements and Data Types - 4.js
+++ Conditional Statements and Data Types - 4.js
@@ -6,7 +6,11 @@ let str = "5";
 let str1 = '';
 let bool = true;
 
+
 console.log(n);
 console.log(+str);
+console.log(str);
 console.log(+str1);
 console.log(+bool);
+console.log(!!n);
+console.log(!!str1);
