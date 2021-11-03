bf11f6e1d404bf3cbb1182e4787e1d62caab9e4b
Артур <87715183+Sanko-Artur@users.noreply.github.com>
2021-11-03 13:36:56 +0300

    Add files via upload

---
+++ Objects and Functions - 1.js
@@ -0,0 +1,20 @@
+/*
+-        Создать объект car, добавить к нему свойство color со значением 'черный'
+-        Изменить свойство color объекта car на 'зеленый'
+-        В объект car добавить свойство power, которое является функцией и выводит в консоль мощность двигателя
+ */
+
+let car = {};
+console.log(car);
+car.color = 'черный';
+console.log(car.color);
+
+car.color = 'зеленый';
+console.log(car.color);
+
+car.power = function() {
+    console.log('Horse power is 55');
+}
+console.log(car);
+
+car.power();
