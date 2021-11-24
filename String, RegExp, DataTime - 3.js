676c7caf68b31d101c2e6817949a6c2f868079a3
Артур <87715183+Sanko-Artur@users.noreply.github.com>
2021-11-24 20:35:17 +0300

    Add files via upload

---
+++ String, RegExp, DataTime - 3.js
@@ -0,0 +1,8 @@
+/*
+Получить день, месяц и год текущей даты и по отдельности вывести в консоль
+ */
+
+console.log ( new Date().getDay() );
+console.log ( new Date().getDate() );
+console.log ( new Date().getMonth() );
+console.log ( new Date().getFullYear() );
